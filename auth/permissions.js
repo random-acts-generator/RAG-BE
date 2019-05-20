aa96d0cf73e5a0f7ee3be94a79c1ac87a8efc34e
Adetunji <ashennaike@gmail.com>
2019-05-20 18:46:16 -0400

    the permissions for authentication is done

--- auth/permissions.js
+++ auth/permissions.js
@@ -4,3 +4,27 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const key = process.env.SECRET
 
+module.exports = {
+  authentication
+}
+
+//the authentication function 
+function permissions(req, res, next) {
+  //grab permission from the headers
+  const token = req.get('Permission');
+
+  if(token) {
+    jwt.verify(token, ket, (err, decode) => {
+      if(err) return res.status(401).json(err);
+
+      req.decode = decode;
+
+      next();
+    })
+  }
+  else {
+    return res.status(401).json({
+      message: 'Please provide a token/key for permission to enter this area.'
+    })
+  }
+}
