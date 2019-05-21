416ad5f85761394b2ae1eb9a8b4082d810f8ac21
Adetunji <ashennaike@gmail.com>
2019-05-21 14:50:25 -0400

    finished migration tables, most likely

---
+++ data/migrations/20190521134334_create_action_table.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('acts', tbl => {
+    tbl.increments();
+
+    // nullable data 
+    tbl
+    .string('description')
+
+    //foreign key
+    tbl
+    .integer('related to')
+    .unsigned()
+    .notNullable()
+    .references('relation')
+    .inTable('contacts')
+    .onDelete('SET NULL')
+    .onUpdate('CASCADE')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('acts')
+};
